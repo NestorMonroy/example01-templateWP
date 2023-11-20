7a7b733869688a140beb36a1c50bf7915e90e6fd
NestorMonroy <nestor.monroy.90@gmail.com>
2023-11-20 17:40:22 -0600

    [add] helpers files

--- themes/pruebas/admin/class-atr-admin.php
+++ themes/pruebas/admin/class-atr-admin.php
@@ -1,6 +1,12 @@
 <?php
 
-class ATR_Admin(
+class ATR_Admin {
+	private $theme_name;
+	private $version;
 
+	function __construct($theme_name, $version){
+		$this -> $theme_name;
+		$this -> $version;
 
-)
+	}
+}
